85c03c6bc25f15e46b1793ffd3b1913d99707b67
Raphael Barreto <tnt.raphael@gmail.com>
2020-02-07 00:55:22 -0300

    Campos virtuais no agendamento

--- modulo-03/src/app/controllers/AppointmentController.js
+++ modulo-03/src/app/controllers/AppointmentController.js
@@ -17,7 +17,7 @@ class AppointmentController {
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
 
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
