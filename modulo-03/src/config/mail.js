57b7e4bdf0b8497bdc172ccbe06785cd9463caf3
Raphael Barreto <tnt.raphael@gmail.com>
2020-02-06 21:24:06 -0300

    Configurando Nodemailer

---
+++ modulo-03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f9e8d953d7a785',
+    pass: 'c1be3a8abe30a0',
+  },
+  default: {
+    from: 'Equipe GoBarber<noreply@gobarber.com>',
+  },
+};
