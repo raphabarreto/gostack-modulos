7c95855f5e561f83b46c16aa12b58b329b01f0c8
Raphael Barreto <tnt.raphael@gmail.com>
2020-02-14 18:12:31 -0300

    Configurando Reactotron

--- modulo-06/src/index.js
+++ modulo-06/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import './config/ReactotronConfig';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -15,10 +17,12 @@ const styles = StyleSheet.create({
   },
 });
 
+console.tron.log('Hello, World');
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.welcome}>React Native!</Text>
       <Text style={styles.welcome}>Abaixo</Text>
     </View>
   );
