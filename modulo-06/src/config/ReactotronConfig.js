fd2e993c0efbece8c61357b1c06a296000dc652d
Raphael Barreto <tnt.raphael@gmail.com>
2020-02-15 10:15:42 -0300

    Adicionado AsyncStorage para conexão do Reactotron

--- modulo-06/src/config/ReactotronConfig.js
+++ modulo-06/src/config/ReactotronConfig.js
@@ -1,8 +1,10 @@
 import Reactotron from 'reactotron-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
   const tron = Reactotron.configure({ host: '192.168.0.2' })
     .useReactNative()
+    .setAsyncStorageHandler(AsyncStorage)
     .connect();
 
   console.tron = tron;
