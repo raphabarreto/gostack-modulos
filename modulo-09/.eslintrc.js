5977f0cdb6da3672c17dec3e59b9164b400458e3
Raphael Barreto <tnt.raphael@gmail.com>
2020-02-24 19:15:50 -0300

    Configurando rotas

--- modulo-09/.eslintrc.js
+++ modulo-09/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     jest: true,
     browser: true
   },
-  extends: ["airbnb", "prettier", "prettier/react"],
+  extends: ["airbnb", "prettier", "prettier/react", "plugin:react/recommended"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
