722e57f241adbad238936aa92cdc20098a589148
Raphael Barreto <tnt.raphael@gmail.com>
2020-03-09 11:34:04 -0300

    Configurando background

--- modulo-10/gobarber/src/pages/SignUp/index.js
+++ modulo-10/gobarber/src/pages/SignUp/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
-export default function SignOut() {
-  return <View />;
+export default function SignUp() {
+  return (
+    <Background>
+      <Text>SignUp</Text>
+    </Background>
+  );
 }
