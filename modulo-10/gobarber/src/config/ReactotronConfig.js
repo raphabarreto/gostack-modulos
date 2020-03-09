9413028f3aa962b0229118d4e1cb61592d1c7e7e
Raphael Barreto <tnt.raphael@gmail.com>
2020-03-09 14:58:52 -0300

    Configurando Redux

--- modulo-10/gobarber/src/config/ReactotronConfig.js
+++ modulo-10/gobarber/src/config/ReactotronConfig.js
@@ -4,11 +4,11 @@ import reactotronSaga from 'reactotron-redux-saga';
 import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '192.168.0.2' })
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure({ host: '192.168.0.2' })
     .use(reactotronRedux())
     .use(reactotronSaga())
     .useReactNative()
-    .setAsyncStorageHandler(AsyncStorage)
     .connect();
 
   console.tron = tron;
