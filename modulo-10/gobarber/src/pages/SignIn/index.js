ea3b1e33df4ffa37887291f9dbb4bdb116426798
Raphael Barreto <tnt.raphael@gmail.com>
2020-03-09 12:49:26 -0300

    Input & Button

--- modulo-10/gobarber/src/pages/SignIn/index.js
+++ modulo-10/gobarber/src/pages/SignIn/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite o seu nome"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
