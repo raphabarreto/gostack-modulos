595e88bca777272f6ce24be90723a40382518873
Raphael Barreto <tnt.raphael@gmail.com>
2020-04-20 15:07:18 -0300

    Primeiro teste

---
+++ modulo-12/src/components/TechList/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+// import { Container } from './styles';
+
+export default function TechList() {
+  const [techs, setTechs] = useState([]);
+
+  function handleAddTechs() {
+    setTechs([...techs, 'Node.js']);
+  }
+  return (
+    <div>
+      <ul data-testid="tech-list">
+        {techs.map(tech => <li key={tech}>{tech}</li>)}
+      </ul>
+      <button onClick={handleAddTechs}>Adicionar</button>
+    </div>
+  );
+}
